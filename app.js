9084de75eb028b18c761b90cae443b7173e5a11e
Okponobi Emmanuel <eaglesemmanuel@outlook.com>
2020-02-10 22:20:17 +0100

    New Changes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
-
+const bookRoute = express.Router();
 const port = process.env.PORT || 2323;
+
+
+bookRoute.route('/books')
+  .get((req, res) => {
+    const request = {hello: "Welcome to my book API"};
+    res.json(request);
+  });
+app.use('/api', bookRoute);
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodemon API!!');
